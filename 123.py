64212b9a5faf420d606e4d79d354e124caa42713
Rita Liu <milkhome0430@gmail.com>
2024-07-02 16:51:28 +0800

    整理程式碼

--- 123.py
+++
@@ -1,6 +0,0 @@
-price = 10 
-pen = 0.38
-rain = True
-name = 'RRRR'
-
-print(price, pen, rain, name)
---
+++ sum.py
@@ -0,0 +1,22 @@
+# data types (種類)
+
+#資料型別
+
+#1. 整數 integer int
+#2. 浮點數(小數) float
+#3. 布林值 boolean bool (True,False)
+#4. 字串 string str(文字) 'Rita' "Rita Liu"
+
+price = 10 
+pen = 0.38
+rain = True
+name = 'Rita'
+
+print(price, pen, rain, name)
+
+name = input ("請輸入名字 : ")
+print("hi " , name)
+
+
+x = 5
+print(x)
