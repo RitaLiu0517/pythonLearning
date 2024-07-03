38959ad82b9125dfa29eb8ff742d6d9b3447ebf9
Rita Liu <milkhome0430@gmail.com>
2024-07-03 15:13:25 +0800

    計算留言長度

--- read_message.py
+++ read_message.py
@@ -6,5 +6,12 @@ with open('reviews.txt','r') as f:
         count +=1
         if(count % 1000 == 0 ): #每一千筆印出一次讀取進度
             print(len(data)) 
-print(len(data))
+print('檔案讀取完畢,共有', len(data), "筆資料")
 print(data[0]) #印出第一筆留言
+
+
+#算留言的平均長度
+sum_len = 0
+for d in data :
+    sum_len += len(d) #每讀取到一筆留言的長度將它加進sum_len裡
+print("留言的平均長度為" ,sum_len/len(data))
