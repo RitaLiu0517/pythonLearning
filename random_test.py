36f516e6bda5f5916c0da09cda1ea04cf7b18b2b
Rita Liu <milkhome0430@gmail.com>
2024-07-03 13:36:16 +0800

    更新

--- random_test.py
+++ random_test.py
@@ -2,14 +2,17 @@ import random
 
 r = random.randint(1,100) #1-100數
 print(r)
+count = 0
+
 while True :
+    count += 1 
     num = int(input("請輸入1-100的數字： "))
     if num > r :
         print ("數字太大囉")
     elif num < r :
         print("數字太小囉")
     else :
-        print("終於猜對囉")
+        print("終於猜對囉，共猜了" , count ,"次")
         break
 
 
