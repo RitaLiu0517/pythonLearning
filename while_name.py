dfec7034fde486c40631a2d446a5f68de4712309
Rita Liu <milkhome0430@gmail.com>
2024-07-03 14:17:47 +0800

    while練習

---
+++ while_name.py
@@ -0,0 +1,4 @@
+while True :
+    name = input( "請輸入你的名字，或輸入q離開: ")
+    if name == 'q' :
+        break
---
+++ while_true.py
@@ -0,0 +1,12 @@
+while True :
+    mode = input("請輸入遊戲模式： ")
+    if mode == "q" :
+        break
+    elif mode == "1" :
+        print("啟動模式一")
+        break
+    elif mode == "2" :
+        print("啟動模式二")
+        break
+    else :
+        print("只能輸入 1/2/q")
