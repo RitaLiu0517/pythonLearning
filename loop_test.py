6000c9217daf990f4b38d3f3b9432091e188079a
Rita Liu <milkhome0430@gmail.com>
2024-07-03 14:12:06 +0800

    forLoop練習

---
+++ loop_test.py
@@ -0,0 +1,24 @@
+#for loop 把清單中的東西一個一個拿出來
+
+#cars = ['MAZDA','NISSAN']
+#for car in cars :  #car 暫時用來稱呼清單中的每一個物件
+#    print(car)
+
+
+
+
+students = ['Allen', 'Tom', 'Jay', 'Amy', 'Summer', 'Rita' ]
+
+for student in students :
+    print("hi" , student)
+
+
+# for loop 解讀字串
+car = 'MAZDA'
+for c in car:
+    print(c) # M A Z D A
+
+print(len(car)) #5
+print('M' in car) #True
+print('MA' in car) #True
+print('S' in car) #False
