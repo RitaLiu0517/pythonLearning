d90346716da924b9eec28793548f60be92ce48fc
Rita Liu <milkhome0430@gmail.com>
2024-07-04 12:47:45 +0800

    整理程式碼

--- read_products.py
+++ read_products.py
@@ -2,20 +2,20 @@
 products = [] 
 with open('products.csv', 'r', encoding='utf-8') as f :
       for line in f :
+            if '商品,價格' in line:
+                  continue #跳過
             name, price = line.strip().split(",")
             products.append([name, price])
 print(products)
-            
 
+#使用者輸入商品及價格       
 while True:
         name = input("請輸入商品名稱：")
         if name == 'q':
             break
-
         price = input("請輸入商品價格：")
         products.append([name, price])
-
-# print(products)
+print(products)
 
 # #將明細寫入電腦檔案
 with open('products.csv','w',encoding='utf-8') as f : #'r'是讀，'W'是寫  也可以用'products.txt'，但是'products.csv'
