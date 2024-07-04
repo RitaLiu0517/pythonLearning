dae49731cb081fb97a0035779189d171429cfbdf
Rita Liu <milkhome0430@gmail.com>
2024-07-04 12:22:52 +0800

    讀取商品紀錄並新增

---
+++ read_products.py
@@ -0,0 +1,25 @@
+#讀取檔案
+products = [] 
+with open('products.csv', 'r', encoding='utf-8') as f :
+      for line in f :
+            name, price = line.strip().split(",")
+            products.append([name, price])
+print(products)
+            
+
+while True:
+        name = input("請輸入商品名稱：")
+        if name == 'q':
+            break
+
+        price = input("請輸入商品價格：")
+        products.append([name, price])
+
+# print(products)
+
+# #將明細寫入電腦檔案
+with open('products.csv','w',encoding='utf-8') as f : #'r'是讀，'W'是寫  也可以用'products.txt'，但是'products.csv'
+      f.write('商品,價格\n') #加入標頭
+      for product in products:
+            f.write(product[0]+ ','+ product[1]+ '\n') #f.write 這段才是真正寫入檔案
+print("最後清單內：",products)
