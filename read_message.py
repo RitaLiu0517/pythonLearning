c31ba3aa92d16bd14fd490764e6ea4786e7ae8c8
Rita Liu <milkhome0430@gmail.com>
2024-07-05 14:25:28 +0800

    使用使用progressbar套件，改用進度條顯示

--- read_message.py
+++ read_message.py
@@ -1,33 +1,32 @@
+import time
+import progressbar
+
 data =[]
 count = 0 
+bar = progressbar.ProgressBar(max_value=1000000) # 使用 progressbar 套件顯示進度條
 with open('reviews.txt','r') as f:
     for line in f :
         data.append(line)
         count +=1
-        if(count % 1000 == 0 ): #每一千筆印出一次讀取進度
-            print(len(data)) 
+        bar.update(count)
+#         if(count % 1000 == 0 ): #每一千筆印出一次讀取進度
+#             print(len(data)) 
 print('檔案讀取完畢,共有', len(data), "筆資料")
 print(data[0]) #印出第一筆留言
-
-
 #算留言的平均長度
 sum_len = 0
 for d in data :
     sum_len += len(d) #每讀取到一筆留言的長度將它加進sum_len裡
 print("留言的平均長度為" ,sum_len/len(data))
 
-
-
 #清單篩選，少於100字的印出
 new =[]
-
 for d in data :
     if(len(d)<100):
         new.append(d) #少於字數100的留言加入新清單內
         
 print ('共印出',len(new),'筆資料字數少於100')
 print(new[0])
-
 
 #清單篩選，有good字的留言印出
 good =[]
@@ -40,7 +39,6 @@ print(good[0])
 #速寫
 good = [d for d in data if 'good' in d] #33-38的速寫 
 # d in data 如果有good,就將程式中最前面的d加入
-
 bad = ['bad' in d for d in data] #for d in data 找有bad的判斷是在用true/false寫出
 #原寫法
 bad = []
@@ -49,6 +47,7 @@ for d in data:
 
 
 #統計每個字出現幾次
+start_time = time.time()
 wc = {} #使用字典 word_count
 for d in data :
     words = d.split()
@@ -58,6 +57,8 @@ for d in data :
             wc[word] += 1
         else:
             wc[word] = 1 
+end_time = time.time()
+print("共花了", end_time - start_time, "sec" )
     
 for word in wc:
     if wc[word] >= 1000000 :
