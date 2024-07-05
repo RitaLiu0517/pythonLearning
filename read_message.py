b853f2cdb019b7688271d4df9f181f22e5799188
Rita Liu <milkhome0430@gmail.com>
2024-07-05 13:43:36 +0800

    新增文字記數及查找功能

--- read_message.py
+++ read_message.py
@@ -9,6 +9,45 @@ with open('reviews.txt','r') as f:
 print('檔案讀取完畢,共有', len(data), "筆資料")
 print(data[0]) #印出第一筆留言
 
+
+#算留言的平均長度
+sum_len = 0
+for d in data :
+    sum_len += len(d) #每讀取到一筆留言的長度將它加進sum_len裡
+print("留言的平均長度為" ,sum_len/len(data))
+
+
+
+#清單篩選，少於100字的印出
+new =[]
+
+for d in data :
+    if(len(d)<100):
+        new.append(d) #少於字數100的留言加入新清單內
+        
+print ('共印出',len(new),'筆資料字數少於100')
+print(new[0])
+
+
+#清單篩選，有good字的留言印出
+good =[]
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print("一共有",len(good),'筆留言提到good')
+print(good[0])
+
+#速寫
+good = [d for d in data if 'good' in d] #33-38的速寫 
+# d in data 如果有good,就將程式中最前面的d加入
+
+bad = ['bad' in d for d in data] #for d in data 找有bad的判斷是在用true/false寫出
+#原寫法
+bad = []
+for d in data: 
+    bad.append('bad' in d) #('bad' in d)是判斷式
+
+
 #統計每個字出現幾次
 wc = {} #使用字典 word_count
 for d in data :
@@ -34,44 +73,3 @@ while True:
     else:
         print(word, "未出現在留言裡")
 print('感謝使用查找系統')
-
-
-
-# #算留言的平均長度
-# sum_len = 0
-# for d in data :
-#     sum_len += len(d) #每讀取到一筆留言的長度將它加進sum_len裡
-# print("留言的平均長度為" ,sum_len/len(data))
-
-
-
-# #清單篩選，少於100字的印出
-# new =[]
-
-# for d in data :
-#     if(len(d)<100):
-#         new.append(d) #少於字數100的留言加入新清單內
-        
-# print ('共印出',len(new),'筆資料字數少於100')
-# print(new[0])
-
-
-# #清單篩選，有good字的留言印出
-# good =[]
-# for d in data:
-#     if 'good' in d:
-#         good.append(d)
-# print("一共有",len(good),'筆留言提到good')
-# print(good[0])
-
-
-# #速寫
-# good = [d for d in data if 'good' in d] #33-38的速寫 
-# # d in data 如果有good,就將程式中最前面的d加入
-
-
-# bad = ['bad' in d for d in data] #for d in data 找有bad的判斷是在用true/false寫出
-# #原寫法
-# bad = []
-# for d in data: 
-#     bad.append('bad' in d) #('bad' in d)是判斷式
